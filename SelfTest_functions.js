dfc19bdbf1654091cbd86164b6b7a0fefc1d69ba
GuillermoBaldan <guillermobaldan@gmail.com>
2022-01-05 06:56:08 +0100

    saveTest function created!

---
+++ SelfTest_functions.js
@@ -0,0 +1,12 @@
+function saveTest(collectionNumber, question, a, b, c, d, solution) {
+  let test = {
+    question: question,
+    a: a,
+    b: b,
+    c: c,
+    d: d,
+    solution: solution,
+  };
+  allTestsArray[collectionNumber].tests.push(test);
+  localStorage.setItem("SelfTest", JSON.stringify(allTestsArray));
+}
